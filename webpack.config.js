aad5cf65e995e419c50f0b8c431f607d622dd65c
Andrew <andrew@Andrews-MacBook-Pro.local>
2023-10-31 18:11:26 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/src/index.jsx',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html'
+    })
+  ],
+  devServer: {
+    static: path.join(__dirname, 'client'),
+    port: 3000,
+    open: true,
+    hot: true,
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
