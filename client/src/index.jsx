04493032c367bc8fcdd5d22847acc5116b5f699e
Andrew <apwicker@gmail.com>
2023-11-01 14:54:22 -0700

    Co-authored-by: Jimmy Phy <jimmally@users.noreply.github.com>

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,39 +1,41 @@
-import React from "react";
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { store } from './store'; //imported RTK Store
 import { Provider } from 'react-redux'; //imported Provider
 import { useDispatch, useSelector } from 'react-redux'; //importeed dispatch and useSelector
-import { addOneToCount } from './slices/mainSlice' //actions
+import { addOneToCount } from './slices/mainSlice'; //actions
 import { Routes, Route, Link } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
-import Dashboard from "./components/dashboard";
-
-
+import Dashboard from './components/dashboard';
+import App from './App';
 
 export default function App() {
-    const dispatch = useDispatch();
-    const { count } = useSelector((state) => state.main);
+  const dispatch = useDispatch();
+  const { count } = useSelector((state) => state.main);
 
-    return (
-        //login route 
-        //dashboard route
-        //neetcode
-        //technologies
+  return (
+    //login route
+    //dashboard route
+    //neetcode
+    //technologies
 
-        <div>
-            <Routes>
-            {/* <Route path='/dashboard' element={<Dashboard/>}/> */}
-            <Route path='/' element={<Dashboard/>}/>
-            </Routes>
-        </div>
-    )
+    <div>
+      <Routes>
+        {/* <Route path='/dashboard' element={<Dashboard/>}/> */}
+        <Route
+          path="/"
+          element={<Dashboard />}
+        />
+      </Routes>
+    </div>
+  );
 }
 
 const root = createRoot(document.getElementById('root'));
-root.render( 
-    <Provider store={store}>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </Provider>,
-)
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
