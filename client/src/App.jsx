29abfe8451e4d659f8f385a2efdf00f8a2388b8d
Shi Kuang <skuang125@gmail.com>
2023-11-01 20:06:54 -0400

    this is reset

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Dashboard from './components/dashboard';
+import ForgetPW from './components/forgetpw';
+// import ForgetPWLandingPage from './components/forgetpwlandingpage';
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        {/* <Route path='/dashboard' element={<Dashboard/>}/> */}
+        <Route
+          path="/"
+          element={<Dashboard />}
+        />
+        <Route
+          path="/forgetpassword"
+          element={<ForgetPW />}
+        />
+        {/* <Route
+            path="/forgetpwlandingpage"
+            element={<ForgetPWLandingPage />}
+          /> */}
+      </Routes>
+    </>
+  );
+};
+
+export default App;
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import ForgetPW from './components/forgetpw';
 // import ForgetPWLandingPage from './components/forgetpwlandingpage';
