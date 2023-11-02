a06aabef40a2979b963306c5d3ffcd3e6899ceb5
Andrew <apwicker@gmail.com>
2023-11-01 21:55:03 -0700

    Refactor App component routing setup
    - Previously, the App component had commented out routes for Dashboard and ForgetPW components. These routes have been removed in this commit to improve code clarity and maintainability.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,32 +27,3 @@ const App = () => {
 };
 
 export default App;
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import ForgetPW from './components/forgetpw';
-// import ForgetPWLandingPage from './components/forgetpwlandingpage';
-
-const App = () => {
-  return (
-    <>
-      <Routes>
-        {/* <Route path='/dashboard' element={<Dashboard/>}/> */}
-        <Route
-          path="/"
-          element={<Dashboard />}
-        />
-        <Route
-          path="/forgetpassword"
-          element={<ForgetPW />}
-        />
-        {/* <Route
-            path="/forgetpwlandingpage"
-            element={<ForgetPWLandingPage />}
-          /> */}
-      </Routes>
-    </>
-  );
-};
-
-export default App;
