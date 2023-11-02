40d34cc41c588baa1d2e298a50d331284c4c2f43
Shi Kuang <skuang125@gmail.com>
2023-11-01 20:06:54 -0400

    this is reset

---
+++ client/src/components/forgetpw.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
+
+const ForgetPW = () => {
+//   //const navigate = useNavigate();
+// //   const handleClick = async (e) => {
+// //     e.preventDefault();
+// //     const email = e.target.form.email.value;
+// //     try {
+// //       const results = await fetch('/forgetpassword', {
+// //         method: 'POST',
+// //         headers: {
+// //           'Content-type': 'application/json',
+// //         },
+// //         body: JSON.stringify({ email: email }),
+// //       });
+
+// //       if (results.emailExists === false) {
+// //         const response = document.querySelector('#forgetpw-response');
+// //         response.innerHTML = results.message;
+// //       } else navigate('/forgetpwlandingpage');
+// //     } catch (err) {
+// //       console.log(err);
+// //     }
+// //   };
+
+  return (
+    <div>
+      <h1> Forget your password? </h1>
+//       <h3>Please enter the email in you use to sign in</h3>
+//       <h4 id="forgetpw-response"></h4>
+//       {/* <form>
+//         Email:{' '}
+//         <input
+//           id="inputemail"
+//           type="text"
+//           name="email"
+//         ></input>
+//         <input
+//           type="button"
+//           onClick={() => handleClick(e)}
+//         >
+//           reset password
+//         </input>
+//       </form> */}
+//     </div>
+  );
+};
+
+export default ForgetPW;
+
