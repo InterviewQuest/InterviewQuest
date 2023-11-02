b3bca446c70de81e553023654fbbd75ae518db08
Andrew <andrew@Andrews-MacBook-Pro.local>
2023-11-02 13:34:35 -0500

    grid functionality in backend

--- server/controllers/techController.js
+++ server/controllers/techController.js
@@ -7,15 +7,36 @@ config.ssl = {
 };
 const pool = new Pool(config);
 
-// const addTech = async (req, res) => {
-//   const { algorithm, difficulty, description, url } = req.body;
-//   try {
+const addTech = async (req, res) => {
+  const { userId, technology } = req.body;
+  const client = await pool.connect();
 
-//   } catch (err) {
-//     console.log('Error adding algorithm: ', err);
-//     res.status(500).send('Server Error During algoController.addAlgo');
-//   }
-// };
+  try {
+    await client.query('BEGIN');
+    
+    const existingTech = await client.query('SELECT * FROM technologies WHERE technology = $1', [technology]);
+    
+    let techId;
+    if (existingTech.rows.length === 0) {
+      const newTech = await client.query('INSERT INTO technologies (technology) VALUES ($1) RETURNING id', [technology]);
+      techId = newTech.rows[0].id;
+    } else {
+      techId = existingTech.rows[0].id;
+    }
+
+    await client.query('INSERT INTO user_technologies (user_id, technology_id, green, pros, cons, opinion, notes) VALUES ($1, $2, false, null, null, null, null)', [userId, techId]);
+    
+    await client.query('COMMIT');
+    res.status(201).send('Technology added successfully');
+  } catch (err) {
+    await client.query('ROLLBACK');
+    console.error('Error adding technology: ', err);
+    res.status(500).send('Server Error During addTech');
+  } finally {
+    client.release();
+  }
+};
+
 
 const getTech = async (req, res) => {
   try {
@@ -28,10 +49,49 @@ const getTech = async (req, res) => {
       .json({ total: totalTech.rowCount, completed: completedTech.rowCount });
   } catch (err) {
     console.log('Error getting tech: ', err);
-    return res.status(500).send('Server Error During algoController.tech');
+    return res.status(500).send('Server Error During techController.getTech');
   }
 };
 
+const updateGreen = async (req, res) => {
+  const { userId, techId } = req.body;
+
+  try {
+    const updatedTech = await pool.query('SELECT * FROM user_technologies WHERE user_id = $1 AND technology_id = $2', [userId, techId]);
+
+    if (updatedTech.rows.length === 0) {
+      return res.status(404).send('Not found :(');
+    }
+
+    const currentGreenValue = updatedTech.rows[0].green;
+    const newGreenValue = !currentGreenValue;
+
+    await pool.query('UPDATE user_technologies SET green = $1 WHERE user_id = $2 AND technology_id = $3', [newGreenValue, userId, techId]);
+
+    return res.status(200).send('Green property updated successfully');
+  } catch (err) {
+    console.log('Error during updateGreen: ', err);
+    return res.status(500).send('Server error during techController.updateGreen');
+  }
+};
+
+const updateNotes = async (req, res) => {
+  const { userId, techId, notes } = req.body;
+
+  try {
+
+    await pool.query('UPDATE user_technologies SET notes = $1 WHERE user_id = $2 AND technology_id = $3', [notes, userId, techId]);
+    return res.status(200).send('Notes property updated successfully');
+
+  } catch (err) {
+    console.log('Error during updateNotes: ', err);
+    return res.status(500).send('Server error during techController.updateNotes');
+  }
+}
+
 module.exports = {
+  addTech,
   getTech,
+  updateGreen,
+  updateNotes
 };
--- server/routes/tech.js
+++ server/routes/tech.js
@@ -6,5 +6,10 @@ const techController = require('../controllers/techController');
 
 router.get('/getTech', techController.getTech);
 
+router.patch('/updateNotes', techController.updateNotes);
+router.patch('/updateGreen', techController.updateGreen);
+
+router.post('/addTech', techController.addTech);
+
 
 module.exports = router;
