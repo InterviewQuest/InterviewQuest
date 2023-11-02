39052b1d1dec1b08ef522aeaa939b06862e3257f
Jimmy Phy <jimmallly@yahoo.com>
2023-11-01 21:09:01 -0700

    Dashboard in progress

--- client/src/slices/mainSlice.js
+++ client/src/slices/mainSlice.js
@@ -1,48 +1,42 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-export const fetchInfo = createAsyncThunk(
-    'main/ENDPOINT',
-    async (data) => {
-      try {
-        const results = await FUNCTION(data);
-        return results
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  );
-  
+export const fetchInfo = createAsyncThunk('main/ENDPOINT', async (data) => {
+  try {
+    const results = await FUNCTION(data);
+    return results;
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 //Initial State of our application
 const initialState = {
-    count: 0,
-}
+  leetCodeQuestionLink: 'https://leetcode.com/problemset/all/',
+  hasQuestionBeenSent: false,
+  randoNumber: 0,
+};
 
 //Create a slice of the application
 export const mainSlice = createSlice({
-    name: 'main',
-    initialState,
-    //Reducers 
-    reducers: {
-        //actions
-        addOneToCount: (state) => {
-            state.count++;
-        }
+  name: 'main',
+  initialState,
+  //Reducers
+  reducers: {
+    //actions
+    randomQuestion: (state, action) => {
+      const {questionSent} = action.payload;
 
-
+      state.hasQuestionBeenSent = questionSent
     },
+  },
 
-    extraReducers: async (state) => {
-        state.addCase(fetchInfo.fulfilled, (state, action) => {
-            //when promise is fulfilled, then we update state
-
-    })}
-
+  extraReducers: async (state) => {
+    state.addCase(fetchInfo.fulfilled, (state, action) => {
+      //when promise is fulfilled, then we update state
+    });
+  },
 });
-
 
 //Export our actions to be dispatched in our application
 
-export const {
-    addOneToCount,
-
-} = mainSlice.actions;
+export const { randomQuestion } = mainSlice.actions;
--- server/routes/algo.js
+++ server/routes/algo.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const algoController = require('../controllers/algoController');
 
+router.get('/getAlgo', algoController.getAlgo);
+
 router.post('/addAlgo', algoController.addAlgo);
+
+
 
 module.exports = router;
